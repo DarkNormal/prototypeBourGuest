cd87ec1de160dfa440d582872156a3d026705a0c
Mark Lordan <mark.lordan@me.com>
2015-02-01 23:53:06 +0000

    working on changing database for adding new data

---
+++ testLogin/Models/Restaurant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace testLogin.Models
+{
+    public class Restaurant
+    {
+        public int RestaurantID { get; set; }
+        public int latitude { get; set; }
+        public int longitude { get; set; }
+        public string bio { get; set; }
+
+    }
+}

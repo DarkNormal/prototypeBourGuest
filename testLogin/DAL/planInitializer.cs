324e00bcf4a7a02ae0becb0519dfd8e54a47ce2b
Mark Lordan <mark.lordan@me.com>
2015-02-09 22:09:31 +0000

    -correction to configurations of migrations -need to commit for laptop in college tomorrow

--- testLogin/DAL/planInitializer.cs
+++ testLogin/DAL/planInitializer.cs
@@ -7,34 +7,34 @@ using testLogin.Models;
 
 namespace testLogin.DAL
 {
-    public class planInitializer :System.Data.Entity. DropCreateDatabaseIfModelChanges<planContext>
+    public class planInitializer :System.Data.Entity. DropCreateDatabaseAlways<planContext>
     {
         protected override void Seed(planContext context) 
         {
-            //var restaurants = new List<Restaurant>
-            //{
-            //    new Restaurant{restaurantID=1,restaurantName="Good Fellas Pizza Pies",latitude=-6.42,longitude=53.28,wheelchair=true,vegetarian=true,type="Italian",bio="Amazing Italian food from Don Corleone", Email="mark.lordan@me.com"},
-            //    new Restaurant{restaurantID=2,restaurantName="Pita Pan",latitude=-6.25,longitude=53.34, wheelchair=false,vegetarian=true,type="Café",bio="A nice bitta pita bread is always good",Email="robert_kenny@outlook.com"},
-            //    new Restaurant{restaurantID=3,restaurantName="Thai Tanic",latitude=-6.48,longitude=53.29, wheelchair=false,vegetarian=false,type="Thai",bio="Wok fried everything",Email="thomasmurphy21@gmail.com"}
-            //};
-            //restaurants.ForEach(r => context.Restaurants.Add(r));
-            //context.SaveChanges();
+            var restaurants = new List<Restaurant>
+            {
+                new Restaurant{restaurantID=1,restaurantName="Good Fellas Pizza ",latitude=-6.66,longitude=53.28,wheelchair=true,vegetarian=true,type="Italian",bio="Amazing Italian food from Don Corleone", Email="mark.lordan@me.com"},
+                new Restaurant{restaurantID=2,restaurantName="Pita Pan Pita Breadsticks",latitude=-6.25,longitude=53.34, wheelchair=false,vegetarian=true,type="Café",bio="A nice bitta pita bread is always good",Email="robert_kenny@outlook.com"},
+                new Restaurant{restaurantID=3,restaurantName="Thai Tanic",latitude=-6.48,longitude=53.29, wheelchair=false,vegetarian=false,type="Thai",bio="Wok fried everything",Email="thomasmurphy21@gmail.com"}
+            };
+            restaurants.ForEach(r => context.Restaurants.Add(r));
+            context.SaveChanges();
 
-            //var floorplan = new List<Floorplan>
-            //{
-            //    new Floorplan{FloorplanID=1,height=5,width=5,numObjects=10}
-            //};
-            //floorplan.ForEach(r => context.Floorplans.Add(r));
-            //context.SaveChanges();
+            var floorplan = new List<Floorplan>
+            {
+                new Floorplan{FloorplanID=1,height=5,width=5,numObjects=10}
+            };
+            floorplan.ForEach(r => context.Floorplans.Add(r));
+            context.SaveChanges();
 
-            //var tableObjects = new List<tableObject>
-            //{ //objType, 0= chair, 1= square table, 2 = stool, 3 = round table
-            //    new tableObject{tableObjectID=1,xcoord=0,ycoord=0,objType=0,available=true,planID=1},
-            //    new tableObject{tableObjectID=2,xcoord=1,ycoord=0,objType=1,available=true,planID=1},
-            //    new tableObject{tableObjectID=3,xcoord=2,ycoord=0,objType=0,available=true,planID=1},
-            //};
-            //tableObjects.ForEach(r => context.tableObjects.Add(r));
-            //context.SaveChanges();
+            var tableObjects = new List<tableObject>
+            { //objType, 0= chair, 1= square table, 2 = stool, 3 = round table
+                new tableObject{tableObjectID=1,xcoord=0,ycoord=0,objType=0,available=true,planID=1},
+                new tableObject{tableObjectID=2,xcoord=1,ycoord=0,objType=1,available=true,planID=1},
+                new tableObject{tableObjectID=3,xcoord=2,ycoord=0,objType=0,available=true,planID=1},
+            };
+            tableObjects.ForEach(r => context.tableObjects.Add(r));
+            context.SaveChanges();
         }
     }
 }
--- testLogin/Migrations/Configuration.cs
+++ testLogin/Migrations/Configuration.cs
@@ -5,43 +5,18 @@ namespace testLogin.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
     using testLogin.Models;
-    using testLogin.DAL;
     using System.Collections.Generic;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<testLogin.DAL.planContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<testLogin.Models.ApplicationDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
-            ContextKey = "testLogin.DAL.planContext";
         }
 
-        protected override void Seed(testLogin.DAL.planContext context)
+        protected override void Seed(testLogin.Models.ApplicationDbContext context)
         {
-            var restaurants = new List<Restaurant>
-            {
-                new Restaurant{restaurantID=1,restaurantName="Good Fellas Pizza Puns",latitude=-6.42,longitude=53.28,wheelchair=true,vegetarian=true,type="Italian",bio="Amazing Italian food from Don Corleone", Email="mark.lordan@me.com"},
-                new Restaurant{restaurantID=2,restaurantName="Pita Pan",latitude=-6.25,longitude=53.34, wheelchair=false,vegetarian=true,type="Café",bio="A nice bitta pita bread is always good",Email="robert_kenny@outlook.com"},
-                new Restaurant{restaurantID=3,restaurantName="Thai Tanic",latitude=-6.48,longitude=53.29, wheelchair=false,vegetarian=false,type="Thai",bio="Wok fried everything",Email="thomasmurphy21@gmail.com"}
-            };
-            restaurants.ForEach(r => context.Restaurants.Add(r));
-            context.SaveChanges();
-
-            var floorplan = new List<Floorplan>
-            {
-                new Floorplan{FloorplanID=1,height=5,width=5,numObjects=10}
-            };
-            floorplan.ForEach(r => context.Floorplans.Add(r));
-            context.SaveChanges();
-
-            var tableObjects = new List<tableObject>
-            { //objType, 0= chair, 1= square table, 2 = stool, 3 = round table
-                new tableObject{tableObjectID=1,xcoord=0,ycoord=0,objType=0,available=true,planID=1},
-                new tableObject{tableObjectID=2,xcoord=1,ycoord=0,objType=1,available=true,planID=1},
-                new tableObject{tableObjectID=3,xcoord=2,ycoord=0,objType=0,available=true,planID=1},
-            };
-            tableObjects.ForEach(r => context.tableObjects.Add(r));
-            context.SaveChanges();
+            
         }
     }
 }
